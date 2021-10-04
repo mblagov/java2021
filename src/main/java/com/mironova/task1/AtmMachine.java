fcae61915dcac4ee3053cf53fd3a7f14dbc87496
annmironova <saintbrainstew@mail.ru>
2021-10-05 01:02:58 +0300

    Creates AtmMachine class and tests for it

---
+++ src/main/java/com/mironova/task1/AtmMachine.java
@@ -0,0 +1,118 @@
+package com.mironova.task1;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * AtmMachine class implements exchanging a banknote to the sum of values.
+ */
+public class AtmMachine {
+    /**
+     * This is the main method which makes use of getValues method.
+     */
+
+    public static void main(String[] args) {
+        List<List<Long>> result = getValues(System.in, System.out);
+        System.out.println("Number of variants: " + result.size());
+        for (List<Long> list : result) {
+            for (Long l : list) {
+                System.out.print(l + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * GetValues method implements exchanging a banknote to the sum of values.
+     *
+     * @param input1 Input stream.
+     * @param output Output stream.
+     * @return List of lists of exchanging variants.
+     */
+    public static List<List<Long>> getValues(InputStream input1, PrintStream output) {
+        long sumForExchange;
+        output.print("Input values:\n");
+        Scanner scanner = new Scanner(input1);
+        String[] strValues = scanner.nextLine().split(" ");
+        output.print("Input total sum:\n");
+        int flag = 0;
+        if (scanner.hasNextLong()) {
+            sumForExchange = scanner.nextLong();
+            if (sumForExchange <= 0) {
+                output.print("Error! A negative number or zero\n");
+            }
+            flag++;
+            long[] values = new long[strValues.length];
+            try {
+                for (int i = 0; i < strValues.length; i++) {
+                    values[i] = Long.parseLong(strValues[i]);
+                    if (values[i] <= 0) {
+                        flag = 0;
+                        output.print("Error! A negative number or zero through values\n");
+                    }
+                }
+            } catch (NumberFormatException e) {
+                flag = 0;
+                output.print("Error! Not an integer\n");
+            }
+            if (flag == 1) {
+                long[] newValues = changeValues(values);
+                return exchange(newValues, sumForExchange, newValues[newValues.length - 1]);
+            }
+        } else {
+            output.print("Error! Not an integer or too big value\n");
+        }
+        scanner.close();
+        return new ArrayList<>();
+    }
+
+    /**
+     * Exchange method implements exchanging a banknote to the sum of values.
+     *
+     * @param values Array of available values.
+     * @param sumForExchange A total sum.
+     * @param maxValue Maximum value
+     * @return result List of lists of exchanging variants.
+     */
+    public static List<List<Long>> exchange(long[] values, long sumForExchange, long maxValue) {
+        List<List<Long>> result = new ArrayList<>();
+        if (sumForExchange == 0) {
+            result.add(new ArrayList<>());
+        } else {
+            for (int i = values.length; i > 0; i--) {
+                long coin = values[i - 1];
+                if (coin <= sumForExchange && coin <= maxValue) {
+                    for (List<Long> preResult : exchange(values, sumForExchange - coin, coin)) {
+                        List<Long> list = new ArrayList<>();
+                        list.add(coin);
+                        list.addAll(preResult);
+                        result.add(list);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * ChangeValues method implements array sorting and deleting repeated elements.
+     *
+     * @param values Array of values.
+     * @return Sorted array without repeated elements
+     */
+    public static long[] changeValues(long[] values) {
+        Arrays.sort(values);
+        int compare = 0;
+        for (int i = 1; i < values.length; i++) {
+            if (values[compare] != values[i]) {
+                compare++;
+                values[compare] = values[i];
+            }
+        }
+        return Arrays.copyOf(values, compare + 1);
+    }
+}
---
+++ src/test/java/com/mironova/task1/AtmMachineTest.java
@@ -0,0 +1,117 @@
+package com.mironova.task1;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+/**
+ * AtmMachine tester.
+ */
+public class AtmMachineTest {
+
+    @Test
+    public void testExchange1() {
+        long[] values = {3, 4};
+        List<List<Long>> res = new ArrayList<>();
+        List<Long> var1 = new ArrayList<>();
+        var1.add(values[1]);
+        var1.add(values[0]);
+        res.add(var1);
+        var resTest = AtmMachine.exchange(values, 7, values[values.length - 1]);
+        assertEquals(res.size(), resTest.size());
+        assertEquals(res, resTest);
+    }
+
+    @Test
+    public void testExchange2() {
+        long[] values = {1, 3};
+        List<List<Long>> res = new ArrayList<>();
+        List<Long> var1 = new ArrayList<>();
+        var1.add(values[1]);
+        var1.add(values[0]);
+        res.add(var1);
+        List<Long> var2 = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            var2.add(values[0]);
+        }
+        res.add(var2);
+        var resTest = AtmMachine.exchange(values, 4, values[values.length - 1]);
+        assertEquals(res.size(), resTest.size());
+        assertEquals(res, resTest);
+    }
+
+    @Test
+    public void testChangeValues() {
+        long[] values = {3, 5, 1, 1, 5, 3, 3, 3};
+        long[] valuesRes = {1, 3, 5};
+        long[] valuesTest = AtmMachine.changeValues(values);
+        assertArrayEquals(valuesRes, valuesTest);
+    }
+
+    @Test
+    public void getValues1() {
+        String input = "1 2" + System.lineSeparator() + "-1";
+        String expectedOutput = "Input values:\n" + "Input total sum:\n"
+                + "Error! A negative number or zero\n";
+        InputStream stringStream = new java.io.ByteArrayInputStream(input.getBytes());
+        OutputStream outputStream = new java.io.ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        AtmMachine.getValues(stringStream, printStream);
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    public void getValues2() {
+        String input = "1 2" + System.lineSeparator() + "string";
+        String expectedOutput = "Input values:\n" + "Input total sum:\n"
+                + "Error! Not an integer or too big value\n";
+        InputStream stringStream = new ByteArrayInputStream(input.getBytes());
+        OutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        AtmMachine.getValues(stringStream, printStream);
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    public void getValues3() {
+        String input = "1 5" + System.lineSeparator() + "10";
+        String expectedOutput = "Input values:\n" + "Input total sum:\n";
+        InputStream stringStream = new java.io.ByteArrayInputStream(input.getBytes());
+        OutputStream outputStream = new java.io.ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        AtmMachine.getValues(stringStream, printStream);
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    public void getValues4() {
+        String input = "1 -5" + System.lineSeparator() + "10";
+        String expectedOutput = "Input values:\n" + "Input total sum:\n"
+                + "Error! A negative number or zero through values\n";
+        InputStream stringStream = new java.io.ByteArrayInputStream(input.getBytes());
+        OutputStream outputStream = new java.io.ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        AtmMachine.getValues(stringStream, printStream);
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    public void getValues5() {
+        String input = "2.3" + System.lineSeparator() + "10";
+        String expectedOutput = "Input values:\n" + "Input total sum:\n"
+                + "Error! Not an integer\n";
+        InputStream stringStream = new java.io.ByteArrayInputStream(input.getBytes());
+        OutputStream outputStream = new java.io.ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        AtmMachine.getValues(stringStream, printStream);
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+}
